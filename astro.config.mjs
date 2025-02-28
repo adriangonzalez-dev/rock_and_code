314bb959aeb29ee94eed8a2067bc419a1ba0cc7d
adriang <adriang@siainteractive.com>
2025-02-28 01:03:38 -0300

    se modifica para service serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node'
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: netlify({}),
   vite: {
     plugins: [tailwindcss()]
   }
